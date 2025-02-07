6a0d4aca3f73bf10b600cf18d8bce2626440dc92
Théo Johannet <theo.johannet@gmail.com>
2025-02-07 17:30:03 +0100

    update for heroku

--- app.py
+++ app.py
@@ -5,22 +5,31 @@ import nltk
 from nltk.corpus import stopwords as nltk_stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import PorterStemmer
+nltk.download("punkt")
+nltk.download('stopwords')
+nltk.download('punkt_tab')
 from unidecode import unidecode
 import string
+from bs4 import BeautifulSoup
 
 
 def prepro_text(text,
-                stop_words,
-                stemming_ps,
+                stop_words=None,
                 remove_html=True,
                 remove_accents=True,
                 lower=True,
+                replace_html=True,
                 replace_hashtag=True,
                 remove_stopwords=True,
                 remove_punct=True,
-                stemming=True):
-    if remove_html:
-        text = BeautifulSoup(text).text
+                stemming=False,
+                stemming_ps=None):
+    if stop_words is None:
+        stop_words = nltk_stopwords.words('english')
+        stop_words = list(set(stop_words))
+        stop_words.sort()
+    # if remove_html:
+    #     text = BeautifulSoup(text).text
     if remove_accents:
         text = unidecode(text)
     if lower:
@@ -43,23 +52,26 @@ def prepro_text(text,
     return text
 
 
-def pred_text(text, encoder, model, stop_words, stemming_ps):
-    text = prepro_text(text, stop_words, stemming_ps)
+def pred_text(text, encoder, model, stemming_ps, stop_words, stemming=True):
+    text = prepro_text(text, stop_words=stop_words, stemming=stemming, stemming_ps=stemming_ps)
     tokens = encoder.transform([text])
     pred = model.predict(tokens)[0]
+    pred = int(pred)
     return pred
 
 
-# Load model
+# Load stuff
 model_path = 'sentiment_model.p'
 encoder_path = 'sentiment_encoder.pk'
 model = pickle.load(open(model_path, 'rb'))
 encoder = pickle.load(open(encoder_path, 'rb'))
-nltk.download("punkt")
 stemming_ps = PorterStemmer()
 stop_words = nltk_stopwords.words('english')
 stop_words = list(set(stop_words))
 stop_words.sort()
+
+pred = pred_text("This is a test !!! I hate chocolate very much", encoder, model, stemming_ps, stop_words)
+print("pred:", pred)
 
 # create Flask app
 app = Flask(__name__)
@@ -75,9 +87,9 @@ def pred():
     data = request.json
     if "text" not in data:
         return jsonify({"error": "Need text key"}), 400
-    pred = pred_text(data["text"], encoder, model, stop_words, stemming_ps)
+    pred = pred_text(data["text"], encoder, model, stemming_ps, stop_words)
     return jsonify({"sentiment": pred}), 200
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5050)
